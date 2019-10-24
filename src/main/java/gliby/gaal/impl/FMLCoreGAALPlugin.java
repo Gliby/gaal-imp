321c9204c13d9d26c8f6ddeb5ad2bf4ea401a3e2
Gliby <Gliby@users.noreply.github.com>
2019-10-25 01:47:23 +0300

    added platform aganostic .json file with pre-defined tasks for project setup

--- src/main/java/gliby/gaal/impl/FMLCoreGAALPlugin.java
+++ src/main/java/gliby/gaal/impl/FMLCoreGAALPlugin.java
@@ -8,18 +8,21 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.FMLLaunchHandler;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.Side;
 import gliby.gaal.GAALCommonHandler;
 import gliby.gaal.GAALMod;
 
+import java.security.cert.Certificate;
 import java.util.Arrays;
 import java.util.Map;
 
 
 //@IFMLLoadingPlugin.MCVersion("1.7.10")
 //@IFMLLoadingPlugin.TransformerExclusions({"gliby.gaal.impl"})
-@IFMLLoadingPlugin.MCVersion(value = "1.7.10")
-@IFMLLoadingPlugin.DependsOn("forge")
+//@IFMLLoadingPlugin.MCVersion(value = "1.7.10")
+//@IFMLLoadingPlugin.DependsOn("forge")
 public class FMLCoreGAALPlugin implements IFMLLoadingPlugin {
     @Override
     public String[] getASMTransformerClass() {
@@ -28,7 +31,6 @@ public class FMLCoreGAALPlugin implements IFMLLoadingPlugin {
 
     @Override
     public String getModContainerClass() {
-        System.out.println("mod container class: " + GAALModContainer.class.getCanonicalName());
         return GAALModContainer.class.getName();
     }
 
@@ -51,6 +53,7 @@ public class FMLCoreGAALPlugin implements IFMLLoadingPlugin {
 
         public GAALModContainer() {
             super(new ModMetadata());
+
             GAALMod mod = GAALCommonHandler.instance().loadMod();
             if (mod != null) {
                 ModMetadata meta = getMetadata();
@@ -63,6 +66,22 @@ public class FMLCoreGAALPlugin implements IFMLLoadingPlugin {
             }
         }
 
+        @Override
+        public Certificate getSigningCertificate()
+        {
+            if (FMLLaunchHandler.side() != Side.CLIENT)
+                return null;
+
+            try
+            {
+                Class<?> cbr = Class.forName("net.minecraft.client.ClientBrandRetriever", false, getClass().getClassLoader());
+                Certificate[] certificates = cbr.getProtectionDomain().getCodeSource().getCertificates();
+                return certificates != null ? certificates[0] : null;
+            }
+            catch (Exception e){} // Errors don't matter just return null.
+            return null;
+        }
+
         @SubscribeEvent
         public void modConstruction(FMLConstructionEvent evt){
 
@@ -70,7 +89,7 @@ public class FMLCoreGAALPlugin implements IFMLLoadingPlugin {
 
         @SubscribeEvent
         public void preInit(FMLPreInitializationEvent evt) {
-
+            System.out.println("pre-init gaal");
         }
 
         @SubscribeEvent
